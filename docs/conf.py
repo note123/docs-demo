7903cd5e7c31e76a2013f6ef347c157b4b2922df
weijie.zhu@marketin.cn <weijie.zhu@marketin.cn>
2018-02-22 19:57:05 +0800

    Build

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,13 @@ from recommonmark.parser import CommonMarkParser
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ['sphinx.ext.*']
+extensions = [
+    'sphinx.ext.autodoc',
+    'sphinx.ext.intersphinx',
+    'sphinxcontrib.httpdomain',
+    'djangodocs',
+    'doc_extensions',
+]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
